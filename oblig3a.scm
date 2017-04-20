d1524969c8d54b1384e3a6453fc51b6f95beece9
Joshua-Schecker <joshuads@mail.uio.no>
2017-04-20 15:12:15 +0200

    Update oblig3a.scm

--- oblig3a.scm
+++ oblig3a.scm
@@ -1,4 +1,7 @@
-(load "prekoda3a.scm")
+;; =============1===========
+;; -------------a-----------
+
+(load "prekode3a.scm")
 
 ;a1
 
@@ -14,9 +17,30 @@
 (define (stream-to-list liste max)
   (define (stream-iter liste counter)
     (if (or (equal? counter max) (stream-null? liste))
-       '()
-       (cons (stream-car liste) (stream-iter (stream-cdr liste) (+ counter 1)))))
+        '()
+        (cons (stream-car liste) (stream-iter (stream-cdr liste) (+ counter 1)))))
   (stream-iter liste 0))
 
+;; b
+  (define (empty-stream args)
+    (cond 
+      ((null? args) #f)
+      ((stream-null? (car args)) #t)
+      (else (empty-stream (cdr args)))))
+
+(define (stream-map proc . argstreams)
+  (if (empty-stream argstreams)
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+; c
+; Petter har ikke regnet med hva som skjer med uendelige strømmer.
+; Siden memq ser gjennom hele listen for å finne en duplikat,
+; kommer den til å bli fanget opp i en evig løkke.
 
 
+
+  
