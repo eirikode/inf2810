592c89feb30778424263ba321b8b1a07c09edcb5
davifri <davidfrisch92@gmail.com>
2017-04-20 13:13:24 +0200

    Add files via upload

--- oblig3a.scm
+++ oblig3a.scm
@@ -1,2 +1,22 @@
 (load "prekoda3a.scm")
 
+;a1
+
+(define (list-to-stream liste)
+  (define (stream-iter liste)
+    (if(null? liste)
+       '()
+       (cons-stream (car liste) (stream-iter (cdr liste)))))
+  (stream-iter liste))
+
+;a2-3
+
+(define (stream-to-list liste max)
+  (define (stream-iter liste counter)
+    (if (or (equal? counter max) (stream-null? liste))
+       '()
+       (cons (stream-car liste) (stream-iter (stream-cdr liste) (+ counter 1)))))
+  (stream-iter liste 0))
+
+
+
